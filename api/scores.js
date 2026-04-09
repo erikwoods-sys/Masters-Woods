e434c9ab28a3160c85662d1da1b18705e4b5744e
erikwoods-sys <erik_woods@hotmail.com>
2026-04-09 23:22:51 +0200

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const response = await fetch(
+      'https://site.api.espn.com/apis/site/v2/sports/golf/leGolf/scoreboard'
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch scores' });
+  }
+}
 
