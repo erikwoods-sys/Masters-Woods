7d77c1d55067a32935154a7ba3007bd0fcdd4ffe
erikwoods-sys <erik_woods@hotmail.com>
2026-04-09 23:35:40 +0200

    Update scores.js

--- api/scores.js
+++ api/scores.js
@@ -3,7 +3,6 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(
       'https://site.api.espn.com/apis/site/v2/sports/golf/pga/scoreboard'
-
     );
     const data = await response.json();
     res.json(data);
@@ -11,4 +10,3 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'Failed to fetch scores' });
   }
 }
-
